7d28a4d9368ce405e895113096cf45688bbd2229
Pauline Sauget <paulinevsauget@gmail.com>
2019-06-20 14:52:22 -0700

    Card component displays quote & formatted emoji

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,17 +5,26 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
+
+
   render() {
+
+    const displayText = (this.props.text) ? this.props.text : ""
+
+    const displayEmoji = (this.props.emoji) ? emoji.getUnicode(this.props.emoji) : ""
+
+
     return (
       <div className="card">
-        Card
+        { displayText } { displayEmoji }
       </div>
     )
   }
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string,
+  emoji: PropTypes.string
 };
 
 export default Card;
