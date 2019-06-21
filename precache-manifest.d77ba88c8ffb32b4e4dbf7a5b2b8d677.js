a77df0e966e436581008ab053216343752bf515f
Pauline Sauget <paulinevsauget@gmail.com>
2019-06-21 15:27:41 -0700

    Updates

---
+++ precache-manifest.d77ba88c8ffb32b4e4dbf7a5b2b8d677.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21554756677c651be8de066333e20c6f",
+    "url": "/inspiration-board/index.html"
+  },
+  {
+    "revision": "9e010c1a04cefd0f3516",
+    "url": "/inspiration-board/static/css/main.0b570879.chunk.css"
+  },
+  {
+    "revision": "a67b76f5def5404af180",
+    "url": "/inspiration-board/static/js/2.902dc7be.chunk.js"
+  },
+  {
+    "revision": "9e010c1a04cefd0f3516",
+    "url": "/inspiration-board/static/js/main.459be64f.chunk.js"
+  },
+  {
+    "revision": "ff446d0d19ecaeb9679b",
+    "url": "/inspiration-board/static/js/runtime~main.ea10e4b3.js"
+  }
+]);
